f96f8cb32161fe0c5508fc288ecd200c0863a6d7
dsplacemnt <dsplacemnt@gmail.com>
2024-06-24 09:02:21 -0300

    chore: changing stuff

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,6 +1,16 @@
-import { Bell, Building, ChevronsRight } from "lucide-react";
+import {
+  Bell,
+  Building,
+  ChevronsRight,
+  LayoutDashboard,
+  LineChart,
+  Package,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
 
 export default function Nav() {
   return (
@@ -23,18 +33,18 @@ export default function Nav() {
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
-                <Building className="h-4 w-4" />
-                Entities
+                <LayoutDashboard className="h-4 w-4" />
+                Dashboard
               </Link>
-              {/* <Link
+              <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <ShoppingCart className="h-4 w-4" />
                 Orders
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                   6
-                </Badge>
+                </Badge> */}
               </Link>
               <Link
                 href="#"
@@ -56,7 +66,7 @@ export default function Nav() {
               >
                 <LineChart className="h-4 w-4" />
                 Analytics
-              </Link> */}
+              </Link>
             </nav>
           </div>
         </div>
